98067b0af19f8fd87049ef52cf883520c6a8e05b
Fran Iruela <franjid@gmail.com>
2020-06-12 09:36:36 +0200

    Updated db structure

--- sql/db_structure.sql
+++ sql/db_structure.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 5.7.18, for osx10.12 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.19, for osx10.15 (x86_64)
 --
 -- Host: localhost    Database: filmaffinity
 -- ------------------------------------------------------
--- Server version	5.7.12
+-- Server version	8.0.19
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,9 +21,9 @@
 
 DROP TABLE IF EXISTS `actor`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `actor` (
-  `idActor` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `idActor` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`idActor`),
   UNIQUE KEY `actor_name` (`name`)
@@ -36,12 +36,12 @@ CREATE TABLE `actor` (
 
 DROP TABLE IF EXISTS `assocFilmActor`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `assocFilmActor` (
-  `idAssocFilmActor` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `idFilm` int(10) unsigned NOT NULL,
-  `idActor` int(10) unsigned NOT NULL,
-  `relevancePosition` int(10) unsigned NOT NULL,
+  `idAssocFilmActor` int unsigned NOT NULL AUTO_INCREMENT,
+  `idFilm` int unsigned NOT NULL,
+  `idActor` int unsigned NOT NULL,
+  `relevancePosition` int unsigned NOT NULL,
   PRIMARY KEY (`idAssocFilmActor`),
   UNIQUE KEY `unique_film_actor` (`idFilm`,`idActor`),
   KEY `assocFilmActor_fk_idActor_idx` (`idActor`),
@@ -56,12 +56,12 @@ CREATE TABLE `assocFilmActor` (
 
 DROP TABLE IF EXISTS `assocFilmCinematographer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `assocFilmCinematographer` (
-  `idAssocFilmCinematographer` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `idFilm` int(10) unsigned NOT NULL,
-  `idCinematographer` int(10) unsigned NOT NULL,
-  `relevancePosition` int(10) unsigned NOT NULL,
+  `idAssocFilmCinematographer` int unsigned NOT NULL AUTO_INCREMENT,
+  `idFilm` int unsigned NOT NULL,
+  `idCinematographer` int unsigned NOT NULL,
+  `relevancePosition` int unsigned NOT NULL,
   PRIMARY KEY (`idAssocFilmCinematographer`),
   UNIQUE KEY `unique_film_cinematographer` (`idFilm`,`idCinematographer`),
   KEY `assocFilmCinematographer_fk_idCinematographer_idx` (`idCinematographer`),
@@ -76,12 +76,12 @@ CREATE TABLE `assocFilmCinematographer` (
 
 DROP TABLE IF EXISTS `assocFilmDirector`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `assocFilmDirector` (
-  `idAssocFilmDirector` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `idFilm` int(10) unsigned NOT NULL,
-  `idDirector` int(10) unsigned NOT NULL,
-  `relevancePosition` int(10) unsigned NOT NULL,
+  `idAssocFilmDirector` int unsigned NOT NULL AUTO_INCREMENT,
+  `idFilm` int unsigned NOT NULL,
+  `idDirector` int unsigned NOT NULL,
+  `relevancePosition` int unsigned NOT NULL,
   PRIMARY KEY (`idAssocFilmDirector`),
   UNIQUE KEY `unique_film_director` (`idFilm`,`idDirector`),
   KEY `assocFilmDirector_fk_idDirector_idx` (`idDirector`),
@@ -96,12 +96,12 @@ CREATE TABLE `assocFilmDirector` (
 
 DROP TABLE IF EXISTS `assocFilmGenre`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `assocFilmGenre` (
-  `idAssocFilmGenre` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `idFilm` int(10) unsigned NOT NULL,
-  `idGenre` int(10) unsigned NOT NULL,
-  `relevancePosition` int(10) unsigned NOT NULL,
+  `idAssocFilmGenre` int unsigned NOT NULL AUTO_INCREMENT,
+  `idFilm` int unsigned NOT NULL,
+  `idGenre` int unsigned NOT NULL,
+  `relevancePosition` int unsigned NOT NULL,
   PRIMARY KEY (`idAssocFilmGenre`),
   UNIQUE KEY `unique_film_genre` (`idFilm`,`idGenre`),
   KEY `assocFilmGenre_fk_idGenre_idx` (`idGenre`),
@@ -116,12 +116,12 @@ CREATE TABLE `assocFilmGenre` (
 
 DROP TABLE IF EXISTS `assocFilmMusician`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `assocFilmMusician` (
-  `idAssocFilmMusician` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `idFilm` int(10) unsigned NOT NULL,
-  `idMusician` int(10) unsigned NOT NULL,
-  `relevancePosition` int(10) unsigned NOT NULL,
+  `idAssocFilmMusician` int unsigned NOT NULL AUTO_INCREMENT,
+  `idFilm` int unsigned NOT NULL,
+  `idMusician` int unsigned NOT NULL,
+  `relevancePosition` int unsigned NOT NULL,
   PRIMARY KEY (`idAssocFilmMusician`),
   UNIQUE KEY `unique_film_musician` (`idFilm`,`idMusician`),
   KEY `assocFilmMusician_fk_idMusician_idx` (`idMusician`),
@@ -136,12 +136,12 @@ CREATE TABLE `assocFilmMusician` (
 
 DROP TABLE IF EXISTS `assocFilmScreenplayer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `assocFilmScreenplayer` (
-  `idAssocFilmScreenplayer` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `idFilm` int(10) unsigned NOT NULL,
-  `idScreenplayer` int(10) unsigned NOT NULL,
-  `relevancePosition` int(10) unsigned NOT NULL,
+  `idAssocFilmScreenplayer` int unsigned NOT NULL AUTO_INCREMENT,
+  `idFilm` int unsigned NOT NULL,
+  `idScreenplayer` int unsigned NOT NULL,
+  `relevancePosition` int unsigned NOT NULL,
   PRIMARY KEY (`idAssocFilmScreenplayer`),
   UNIQUE KEY `unique_film_screenplayer` (`idFilm`,`idScreenplayer`),
   KEY `assocFilmScreenplayer_fk_idScreenplayer_idx` (`idScreenplayer`),
@@ -156,12 +156,12 @@ CREATE TABLE `assocFilmScreenplayer` (
 
 DROP TABLE IF EXISTS `assocFilmTopic`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `assocFilmTopic` (
-  `idAssocFilmTopic` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `idFilm` int(10) unsigned NOT NULL,
-  `idTopic` int(10) unsigned NOT NULL,
-  `relevancePosition` int(10) unsigned NOT NULL,
+  `idAssocFilmTopic` int unsigned NOT NULL AUTO_INCREMENT,
+  `idFilm` int unsigned NOT NULL,
+  `idTopic` int unsigned NOT NULL,
+  `relevancePosition` int unsigned NOT NULL,
   PRIMARY KEY (`idAssocFilmTopic`),
   UNIQUE KEY `unique_film_topic` (`idFilm`,`idTopic`),
   KEY `assocFilmTopic_fk_idTopic_idx` (`idTopic`),
@@ -176,9 +176,9 @@ CREATE TABLE `assocFilmTopic` (
 
 DROP TABLE IF EXISTS `cinematographer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cinematographer` (
-  `idCinematographer` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `idCinematographer` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`idCinematographer`),
   UNIQUE KEY `cinematographer_name` (`name`)
@@ -191,9 +191,9 @@ CREATE TABLE `cinematographer` (
 
 DROP TABLE IF EXISTS `director`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `director` (
-  `idDirector` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `idDirector` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`idDirector`),
   UNIQUE KEY `director_name` (`name`)
@@ -206,9 +206,9 @@ CREATE TABLE `director` (
 
 DROP TABLE IF EXISTS `failedCrawlFilm`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `failedCrawlFilm` (
-  `idFilm` int(10) unsigned NOT NULL,
+  `idFilm` int unsigned NOT NULL,
   PRIMARY KEY (`idFilm`),
   UNIQUE KEY `idFilm_UNIQUE` (`idFilm`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -220,20 +220,20 @@ CREATE TABLE `failedCrawlFilm` (
 
 DROP TABLE IF EXISTS `film`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `film` (
-  `idFilm` int(10) unsigned NOT NULL,
+  `idFilm` int unsigned NOT NULL,
   `title` varchar(255) NOT NULL,
   `originalTitle` varchar(255) NOT NULL,
   `year` char(4) DEFAULT NULL,
-  `duration` mediumint(8) unsigned DEFAULT NULL,
+  `duration` mediumint unsigned DEFAULT NULL,
   `country` char(2) DEFAULT NULL,
   `producer` text,
   `awards` text,
   `synopsis` text,
   `officialReviews` text,
   `rating` decimal(2,1) unsigned DEFAULT NULL,
-  `numRatings` mediumint(8) unsigned DEFAULT NULL,
+  `numRatings` mediumint unsigned DEFAULT NULL,
   PRIMARY KEY (`idFilm`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -244,13 +244,13 @@ CREATE TABLE `film` (
 
 DROP TABLE IF EXISTS `filmInTheatres`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `filmInTheatres` (
-  `idFilm` int(10) unsigned NOT NULL,
+  `idFilm` int unsigned NOT NULL,
   `releaseDate` date NOT NULL,
   PRIMARY KEY (`idFilm`),
   UNIQUE KEY `idFilm_UNIQUE` (`idFilm`),
-  CONSTRAINT `fk_filmInTheatres_idFilm` FOREIGN KEY (`idFilm`) REFERENCES `film` (`idFilm`) ON DELETE CASCADE ON UPDATE NO ACTION
+  CONSTRAINT `fk_filmInTheatres_idFilm` FOREIGN KEY (`idFilm`) REFERENCES `film` (`idFilm`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -260,13 +260,13 @@ CREATE TABLE `filmInTheatres` (
 
 DROP TABLE IF EXISTS `filmPopular`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `filmPopular` (
-  `idFilm` int(10) unsigned NOT NULL,
-  `ranking` tinyint(1) unsigned NOT NULL,
+  `idFilm` int unsigned NOT NULL,
+  `ranking` tinyint unsigned NOT NULL,
   PRIMARY KEY (`idFilm`),
   UNIQUE KEY `idFilm_UNIQUE` (`idFilm`),
-  CONSTRAINT `fk_filmPopular_idFilm` FOREIGN KEY (`idFilm`) REFERENCES `film` (`idFilm`) ON DELETE CASCADE ON UPDATE NO ACTION
+  CONSTRAINT `fk_filmPopular_idFilm` FOREIGN KEY (`idFilm`) REFERENCES `film` (`idFilm`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -276,9 +276,9 @@ CREATE TABLE `filmPopular` (
 
 DROP TABLE IF EXISTS `genre`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `genre` (
-  `idGenre` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `idGenre` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`idGenre`),
   UNIQUE KEY `genre_name` (`name`)
@@ -291,9 +291,9 @@ CREATE TABLE `genre` (
 
 DROP TABLE IF EXISTS `musician`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `musician` (
-  `idMusician` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `idMusician` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`idMusician`),
   UNIQUE KEY `musician_name` (`name`)
@@ -306,9 +306,9 @@ CREATE TABLE `musician` (
 
 DROP TABLE IF EXISTS `screenplayer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `screenplayer` (
-  `idScreenplayer` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `idScreenplayer` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`idScreenplayer`),
   UNIQUE KEY `screenplayer_name` (`name`)
@@ -321,9 +321,9 @@ CREATE TABLE `screenplayer` (
 
 DROP TABLE IF EXISTS `topic`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `topic` (
-  `idTopic` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `idTopic` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`idTopic`),
   UNIQUE KEY `name_topic` (`name`)
@@ -346,6 +346,10 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `userFriendship`
+--
+
 DROP TABLE IF EXISTS `userFriendship`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -353,8 +357,12 @@ CREATE TABLE `userFriendship` (
   `idUserSource` int unsigned NOT NULL,
   `idUserTarget` int unsigned NOT NULL,
   UNIQUE KEY `source_target` (`idUserSource`,`idUserTarget`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `userRating`
+--
 
 DROP TABLE IF EXISTS `userRating`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -370,9 +378,8 @@ CREATE TABLE `userRating` (
   UNIQUE KEY `idUser_idFilm` (`idUser`,`idFilm`),
   KEY `idUser` (`idUser`),
   KEY `idFilm` (`idFilm`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -383,4 +390,4 @@ CREATE TABLE `userRating` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-03-07 15:07:52
+-- Dump completed on 2020-06-12  9:28:53
