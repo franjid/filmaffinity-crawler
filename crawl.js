9efa950615e183ef6bc4e9a31e59b10755f24af8
Fran Iruela <franjid@gmail.com>
2020-06-24 10:36:06 +0200

    Added full path to log

--- crawl.js
+++ crawl.js
@@ -4,7 +4,7 @@ var Log = require('log');
 
 var arguments = require(__dirname + '/lib/arguments.js');
 
-global.log = new Log('debug', fs.createWriteStream('crawler.log'));
+global.log = new Log('debug', fs.createWriteStream(__dirname + '/crawler.log'));
 
 global.parameters = ini.parse(
     fs.readFileSync(__dirname + '/config/parameters.ini', 'utf-8')
