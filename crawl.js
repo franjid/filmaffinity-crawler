2d4a2d6bd053f9f544960d32241a50e0ef3e206c
Fran Iruela <franjid@gmail.com>
2020-06-24 10:43:02 +0200

    Append logs (before it was truncating the log file)

--- crawl.js
+++ crawl.js
@@ -4,24 +4,31 @@ var Log = require('log');
 
 var arguments = require(__dirname + '/lib/arguments.js');
 
-global.log = new Log('debug', fs.createWriteStream(__dirname + '/crawler.log'));
+global.log = new Log(
+  'debug',
+  fs.createWriteStream(__dirname + '/crawler.log', {
+    flags: 'a',
+    encoding: null,
+    mode: 0666
+  })
+);
 
 global.parameters = ini.parse(
-    fs.readFileSync(__dirname + '/config/parameters.ini', 'utf-8')
+  fs.readFileSync(__dirname + '/config/parameters.ini', 'utf-8')
 ).parameters;
 
 global.filmError = {};
 
-arguments.getAction(function(action, id) {
-    var crawler = require(__dirname + '/lib/actions/' + action + '.js');
+arguments.getAction(function (action, id) {
+  var crawler = require(__dirname + '/lib/actions/' + action + '.js');
 
-    process.on('uncaughtException', function (err) {
-        // console.log('UNCAUGHT EXCEPTION - keeping process alive:', err);
-    });
+  process.on('uncaughtException', function (err) {
+    // console.log('UNCAUGHT EXCEPTION - keeping process alive:', err);
+  });
 
-    if (action === 'user_friends' || action === 'id') {
-        crawler.start(id);
-    } else {
-        crawler.start();
-    }
+  if (action === 'user_friends' || action === 'id') {
+    crawler.start(id);
+  } else {
+    crawler.start();
+  }
 });
